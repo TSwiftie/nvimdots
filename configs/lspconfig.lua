7eb245206c6e4a93712ba5bdd709303f2eb43484
TSwiftie13 <tswiftie@foxmail.com>
2023-04-02 00:28:24 +0800

    feat(lsp): fix mult lsp client error

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -4,14 +4,22 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = {"clangd" }
+local servers = { "clangd" }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+  if lsp == 'clangd' then
+    local clangd_capabilities = capabilities
+    clangd_capabilities.offsetEncoding = { "utf-16" }
+    lspconfig[lsp].setup {
+      on_attach = on_attach,
+      capabilities = clangd_capabilities,
+    }
+  else
+    lspconfig[lsp].setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+    }
+  end
 end
 
--- 
 -- lspconfig.pyright.setup { blabla}
